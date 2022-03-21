107519c51627745f537b4a39f1511ba74ebf091a
daiqin <daiqin@58.com>
2022-03-21 16:56:38 +0800

    feat layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Send from '@/components/Send'
+import logo from '../public/logo.svg'
+import './App.css'
 
 const App: React.FC = () => {
   return (
-    <div>
-      <Send />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>mmc-file-storage-system</p>
+        <p>By: 毛毛虫&可乐加冰</p>
+        <Send />
+      </header>
     </div>
   )
 }
